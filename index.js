c0172465026f126f6a4d9de9e2f06579eb7446cd
nedoratic <nedoratic@gmail.com>
2024-02-15 21:13:07 +0100

    index.js - date elements variables added

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ const dateOptionsSelectElement = document.getElementById('date-options');
 
 // Date Object Variable
 const date = new Date();
+
+// Date Elements Variables
+const day = date.getDate();
+const month = date.getMonth() + 1;
+const year = date.getFullYear();
+const hours = date.getHours();
+const minutes = date.getMinutes();
