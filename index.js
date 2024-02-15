a282371680862ba4a4c419319da48995f13b30f5
nedoratic <nedoratic@gmail.com>
2024-02-15 21:12:18 +0100

    index.js - date object added to date variable

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 // DOM Elements Variables
 const currentDateParagraph = document.getElementById('current-date');
 const dateOptionsSelectElement = document.getElementById('date-options');
+
+// Date Object Variable
+const date = new Date();
