c2836d22da0935877d3a82e1bac5577cafe5d648
nedoratic <nedoratic@gmail.com>
2024-02-16 22:30:33 +0100

    event listener for date options added

--- index.js
+++ index.js
@@ -15,3 +15,17 @@ const minutes = date.getMinutes();
 // Formatted Date Variable
 const formattedDate = `${day}-${month}-${year}`;
 currentDateParagraph.textContent = formattedDate;
+
+// Event Listener For Date Options
+dateOptionsSelectElement.addEventListener('change', () => {
+	switch (dateOptionsSelectElement.value) {
+		case 'yyyy-mm-dd':
+			currentDateParagraph.textContent = formattedDate.split('-').reverse().join('-');
+			break;
+		case 'mm-dd-yyyy-h-mm':
+			currentDateParagraph.textContent = `${month}-${day}-${year} ${hours} Hours ${minutes} Minutes`;
+			break;
+		default:
+			currentDateParagraph.textContent = formattedDate;
+	}
+});
